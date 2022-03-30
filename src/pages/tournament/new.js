6e160cf3b1a179ef0d5fa59ae7f24770e6f94716
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-03-30 23:29:32 +0200

    Created tournament wizard

---
+++ src/pages/tournament/new.js
@@ -0,0 +1,179 @@
+import AuthGuard from "../../guards/AuthGuard";
+import { MainLayout } from "../../components/main-layout";
+import {
+  Container,
+  Grid,
+  StepContent,
+  Step,
+  StepIcon,
+  StepLabel,
+  Stepper,
+  Typography,
+  Avatar,
+  Card,
+  Button,
+} from "@mui/material";
+import Head from "next/head";
+import { Box } from "@mui/system";
+import { TournamentScoreType } from "./tournament-score-type";
+import { Check as CheckIcon } from "../../icons/check";
+import { useState } from "react";
+import { ArrowRight as ArrowRightIcon } from "../../icons/arrow-right";
+import { TournamentFormatType } from "./tournament-format-type";
+import { TournamentData } from "./tournament-data";
+
+const CreateTournament = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [complete, setComplete] = useState(false);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleComplete = () => {
+    setComplete(true);
+  };
+
+  const steps = [
+    {
+      label: "Scoring system",
+      content: <TournamentScoreType onBack={handleBack} onNext={handleNext} />,
+    },
+    {
+      label: "Team format",
+      content: <TournamentFormatType onBack={handleBack} onNext={handleNext} />,
+    },
+    {
+      label: "Tournament data",
+      content: <TournamentData onBack={handleBack} onNext={handleNext} />,
+    },
+    // {
+    //   label: 'Description',
+    //   content: (
+    //     <JobDescriptionStep
+    //       onBack={handleBack}
+    //       onNext={handleComplete}
+    //     />
+    //   )
+    // }
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>New Tournament | Tournabay</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          display: "flex",
+          flexGrow: 1,
+        }}
+      >
+        <Grid container sx={{ flexGrow: 1 }}>
+          <Grid
+            item
+            xs={12}
+            md={12}
+            sx={{
+              pt: {
+                xs: 4,
+                sm: 4,
+                md: 4,
+              },
+              pb: 4,
+            }}
+          >
+            <Box maxWidth="lg">
+              <Typography sx={{ mb: 3 }} variant="h4">
+                Create tournament
+              </Typography>
+              {!complete ? (
+                <Stepper
+                  activeStep={activeStep}
+                  orientation="vertical"
+                  sx={{
+                    "& .MuiStepConnector-line": {
+                      borderLeftColor: "divider",
+                      borderLeftWidth: 2,
+                    },
+                  }}
+                >
+                  {steps.map((step, index) => (
+                    <Step key={step.label}>
+                      <StepLabel StepIconComponent={StepIcon}>
+                        <Typography sx={{ ml: 2 }} variant="overline">
+                          {step.label}
+                        </Typography>
+                      </StepLabel>
+                      <StepContent
+                        sx={{
+                          borderLeftColor: "divider",
+                          borderLeftWidth: 2,
+                          ...(activeStep === index && {
+                            py: 4,
+                          }),
+                        }}
+                      >
+                        {step.content}
+                      </StepContent>
+                    </Step>
+                  ))}
+                </Stepper>
+              ) : (
+                <Box
+                  maxWidth="md"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Avatar
+                    sx={{
+                      backgroundColor: "success.main",
+                      color: "success.contrastText",
+                      height: 40,
+                      width: 40,
+                    }}
+                  >
+                    <CheckIcon />
+                  </Avatar>
+                  <Typography variant="h6" sx={{ mt: 2 }}>
+                    All done!
+                  </Typography>
+                  <Typography color="textSecondary" variant="body2">
+                    You can now manage your newly created tournament
+                  </Typography>
+                  <Button
+                    sx={{ mt: 4, mb: 4 }}
+                    endIcon={<ArrowRightIcon fontSize="small" />}
+                    onClick={() => {}}
+                    variant="contained"
+                  >
+                    Tournament dashboard
+                  </Button>
+                </Box>
+              )}
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+CreateTournament.getLayout = (page) => (
+  <AuthGuard>
+    <MainLayout>
+      <Container maxWidth="lg">{page}</Container>
+    </MainLayout>
+  </AuthGuard>
+);
+
+export default CreateTournament;
