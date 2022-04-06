76dc0ad6f325cd7afcd0ef1e4d22296a3e3db6f3
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-04-06 19:32:39 +0200

    Added tournamentWizard to Redux

---
+++ src/slices/tournamentWizard.js
@@ -0,0 +1,119 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { apolloClient } from "../apollo/apolloClient";
+import { createTournamentMutation } from "../ql/TournamentMutations";
+import { setTournament } from "./tournament";
+
+const initialState = {
+  tournament: {
+    name: "",
+    scoreType: "SCORE_V2",
+    gameMode: "STANDARD",
+    teamFormat: "PLAYER_VS",
+    startDate: new Date(),
+    endDate: new Date(),
+  },
+};
+
+const slice = createSlice({
+  name: "tournamentWizard",
+  initialState,
+  reducers: {
+    reset(state, action) {
+      state.tournament = initialState;
+    },
+    setTournamentId(state, action) {
+      state.tournament.id = action.payload;
+    },
+    setScoreType(state, action) {
+      state.tournament.scoreType = action.payload;
+    },
+    setGameMode(state, action) {
+      state.tournament.gameMode = action.payload;
+    },
+    setName(state, action) {
+      state.tournament.name = action.payload;
+    },
+    setTournamentStartDate(state, action) {
+      state.tournament.startDate = action.payload;
+    },
+    setTournamentEndDate(state, action) {
+      state.tournament.endDate = action.payload;
+    },
+    setTournamentTeamFormat(state, action) {
+      state.tournament.teamFormat = action.payload;
+    },
+  },
+});
+
+export const { reducer } = slice;
+
+// actions
+
+export const resetWizard = () => async (dispatch) => {
+  dispatch(slice.actions.reset());
+};
+
+export const setTournamentId = (id) => async (dispatch) => {
+  dispatch(slice.actions.setTournamentId(id));
+};
+
+export const setTournamentScoreType = (scoreType) => async (dispatch) => {
+  dispatch(slice.actions.setScoreType(scoreType));
+};
+
+export const setTournamentName = (name) => async (dispatch) => {
+  dispatch(slice.actions.setName(name));
+};
+
+export const setTournamentGameMode = (gameMode) => async (dispatch) => {
+  dispatch(slice.actions.setGameMode(gameMode));
+};
+
+export const setTournamentStartDate = (startDate) => async (dispatch) => {
+  dispatch(slice.actions.setTournamentStartDate(startDate));
+};
+
+export const setTournamentEndDate = (endDate) => async (dispatch) => {
+  dispatch(slice.actions.setTournamentEndDate(endDate));
+};
+
+export const setTournamentTeamFormat = (teamFormat) => async (dispatch) => {
+  dispatch(slice.actions.setTournamentTeamFormat(teamFormat));
+};
+
+export const createTournament = (tournamentData) => async (dispatch) => {
+  let tournament = await apolloClient.mutate({
+    mutation: createTournamentMutation,
+    variables: {
+      input: tournamentData,
+    },
+  });
+  const {
+    id,
+    name,
+    createdAt,
+    updatedAt,
+    startDate,
+    endDate,
+    gameMode,
+    scoreType,
+    teamFormat,
+    owner,
+  } = tournament.data;
+  return await dispatch(
+    setTournament({
+      id,
+      name,
+      createdAt,
+      updatedAt,
+      startDate,
+      endDate,
+      gameMode,
+      scoreType,
+      teamFormat,
+      owner,
+    })
+  );
+};
+
+export default slice;
