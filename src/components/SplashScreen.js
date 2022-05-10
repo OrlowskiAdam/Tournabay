c4f1ce07d0b356d55787a1b788db858a2e0628c9
adam.orlowski <CvPGrvHagjHf0DKZ0M41@>
2022-05-10 11:24:09 +0200

    Updated ci/cd

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,5 +1,25 @@
 import { Box } from "@mui/material";
 
-const SlashScreen = () => <Box>Loading...</Box>;
+const SlashScreen = () => (
+  <Box
+    sx={{
+      alignItems: "center",
+      backgroundColor: "background.paper",
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+      justifyContent: "center",
+      left: 0,
+      p: 3,
+      position: "fixed",
+      top: 0,
+      width: "100%",
+      zIndex: 2000,
+    }}
+  >
+    {/*<Logo />*/}
+    Loading...
+  </Box>
+);
 
 export default SlashScreen;
