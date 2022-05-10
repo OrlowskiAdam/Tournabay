ca020ddf3364b242df28762b8b1d578d56ecc639
adam.orlowski <CvPGrvHagjHf0DKZ0M41@>
2022-05-10 08:08:20 +0200

    Added Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,41 @@
 import { MainLayout } from "../components/main-layout";
 import { Container } from "@mui/material";
 import useOsuAuth from "../hooks/useOsuAuth";
+import { Head } from 'next/document';
 
 const HomePage = () => {
   const { user } = useOsuAuth();
 
-  return <Container maxWidth="lg">{user.username}</Container>;
+  return (
+    <>
+      <Head>
+        <!-- Primary Meta Tags -->
+        <title>Tournabay (early Alpha)</title>
+        <meta name="title" content="Tournabay"/>
+        <meta name="description"
+              content="(closed Alpha) Tournabay management system for osu! cummunity tournaments. This project is still in work and will be released in the near future."/>
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://tournabay.com/"/>
+        <meta property="og:title" content="Tournabay (early Alpha)"/>
+        <meta property="og:description"
+              content="Tournabay management system for osu! cummunity tournaments."/>
+        <meta property="og:image"
+              content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://tournabay.com/"/>
+        <meta property="twitter:title" content="Tournabay (early Alpha)"/>
+        <meta property="twitter:description"
+              content="Tournabay management system for osu! cummunity tournaments."/>
+        <meta property="twitter:image"
+              content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+      </Head>
+      <Container maxWidth="lg">{user.username}</Container>
+    </>
+  );
 };
 
 HomePage.getLayout = (page) => <MainLayout>{page}</MainLayout>;
