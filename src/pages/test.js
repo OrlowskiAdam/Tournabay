035bcde4e4054302d254cfab02e3d84a8978a897
adam.orlowski <CvPGrvHagjHf0DKZ0M41@>
2022-05-10 11:19:59 +0200

    Head test without suspense

---
+++ src/pages/test.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Test = () => {
+  return (
+    <>
+      <Head>
+        <title>Test test</title>
+        <meta
+          property="og:description"
+          content="Tournabay management system for osu! cummunity tournaments.:index:24"
+        />
+        <meta name="theme-color" content="#466eff" />
+      </Head>
+      <h1>Testtest</h1>
+    </>
+  );
+};
+
+export default Test;
