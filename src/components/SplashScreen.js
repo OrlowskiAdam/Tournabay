e0af161ca1ba22b9eaf9afa3ba0c9bcb03c85c72
adam.orlowski <CvPGrvHagjHf0DKZ0M41@>
2022-05-10 08:37:33 +0200

    Fixed wrong Head import

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,25 +1,60 @@
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
+import Head from "next/head";
 
 const SlashScreen = () => (
-  <Box
-    sx={{
-      alignItems: 'center',
-      backgroundColor: 'background.paper',
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100%',
-      justifyContent: 'center',
-      left: 0,
-      p: 3,
-      position: 'fixed',
-      top: 0,
-      width: '100%',
-      zIndex: 2000
-    }}
-  >
-    {/*<Logo />*/}
-    Loading...
-  </Box>
+  <>
+    <Head>
+      <title>Tournabay (early Alpha)</title>
+      <meta name="title" content="Tournabay" />
+      <meta
+        name="description"
+        content="(closed Alpha) Tournabay management system for osu! cummunity tournaments. This project is still in work and will be released in the near future."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tournabay.com/" />
+      <meta property="og:title" content="Tournabay (early Alpha)" />
+      <meta
+        property="og:description"
+        content="Tournabay management system for osu! cummunity tournaments."
+      />
+      <meta
+        property="og:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://tournabay.com/" />
+      <meta property="twitter:title" content="Tournabay (early Alpha)" />
+      <meta
+        property="twitter:description"
+        content="Tournabay management system for osu! cummunity tournaments."
+      />
+      <meta
+        property="twitter:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+    </Head>
+    <Box
+      sx={{
+        alignItems: "center",
+        backgroundColor: "background.paper",
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        justifyContent: "center",
+        left: 0,
+        p: 3,
+        position: "fixed",
+        top: 0,
+        width: "100%",
+        zIndex: 2000,
+      }}
+    >
+      {/*<Logo />*/}
+      Loading...
+    </Box>
+  </>
 );
 
 export default SlashScreen;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { MainLayout } from "../components/main-layout";
 import { Container } from "@mui/material";
 import useOsuAuth from "../hooks/useOsuAuth";
-import { Head } from 'next/document';
+import Head from "next/head";
 
 const HomePage = () => {
   const { user } = useOsuAuth();
@@ -10,25 +10,35 @@ const HomePage = () => {
     <>
       <Head>
         <title>Tournabay (early Alpha)</title>
-        <meta name="title" content="Tournabay"/>
-        <meta name="description"
-              content="(closed Alpha) Tournabay management system for osu! cummunity tournaments. This project is still in work and will be released in the near future."/>
+        <meta name="title" content="Tournabay" />
+        <meta
+          name="description"
+          content="(closed Alpha) Tournabay management system for osu! cummunity tournaments. This project is still in work and will be released in the near future."
+        />
 
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://tournabay.com/"/>
-        <meta property="og:title" content="Tournabay (early Alpha)"/>
-        <meta property="og:description"
-              content="Tournabay management system for osu! cummunity tournaments."/>
-        <meta property="og:image"
-              content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tournabay.com/" />
+        <meta property="og:title" content="Tournabay (early Alpha)" />
+        <meta
+          property="og:description"
+          content="Tournabay management system for osu! cummunity tournaments."
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
 
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://tournabay.com/"/>
-        <meta property="twitter:title" content="Tournabay (early Alpha)"/>
-        <meta property="twitter:description"
-              content="Tournabay management system for osu! cummunity tournaments."/>
-        <meta property="twitter:image"
-              content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://tournabay.com/" />
+        <meta property="twitter:title" content="Tournabay (early Alpha)" />
+        <meta
+          property="twitter:description"
+          content="Tournabay management system for osu! cummunity tournaments."
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
       </Head>
       <Container maxWidth="lg">{user.username}</Container>
     </>
